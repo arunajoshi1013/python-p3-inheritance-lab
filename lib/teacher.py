5a944542521f841bc3b0afc7aeea1b398df08c1c
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-08-16 21:42:14 -0700

    inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,9 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, fname, lname, knowledge):
+        super().__init__(fname, lname)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, self.knowledge.__len__())]
