f4ada2cb52ba026325477fbd3058e4b4c2ec20e4
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-08-16 21:46:52 -0700

    fix inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,9 +5,18 @@ from user import User
 import random
 
 class Teacher(User):
-    def __init__(self, fname, lname, knowledge):
+    def __init__(self, fname, lname):
         super().__init__(fname, lname)
-        self.knowledge = knowledge
+        self.knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]
 
     def teach(self):
         return self.knowledge[random.randint(0, self.knowledge.__len__())]
